c0fcb9a2bfc71e40b50e2a8efe7eedeaa52f5a69
Leonardo Furmann <leohitnerf@gmail.com>
2024-09-19 18:57:48 -0300

    fetch produtos

--- projeto_routes/src/componentes/Produtos.jsx
+++ projeto_routes/src/componentes/Produtos.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import styles  from './Produtos.module.css'
+import { Link } from 'react-router-dom';
+// https://ranekapi.origamid.dev/json/api/produto
 
 const Produtos = () => {
+
+  const [produtos, setProdutos] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+    .then(data => data.json()).then(json => setProdutos(json))
+}, [])
+
+ if (produtos == null) return null;
+  
   return (
-    <div>Produtos</div>
+    <section className={styles.produtos + ' animeLeft'}>
+      {produtos.map((p) => (
+        <Link key={p.id}>
+            {/* <img src={p.fotos[0].scr} alt={p.fotos[0].titulo} /> */}
+            <h1 className={styles.nome}>{p.nome}</h1>
+        </Link>
+      ))}
+    </section>
   )
 }
 
