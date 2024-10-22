f697ad18e199ac0d5b2cc0a42f689bc8854e6503
Leonardo Furmann <leohitnerf@gmail.com>
2024-10-22 18:55:14 -0300

    useContext

--- dogs/src/Components/Login/LoginForm.jsx
+++ dogs/src/Components/Login/LoginForm.jsx
@@ -1,44 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Input from '../Form/Input';
 import Button from '../Form/Button';
 import useForm from '../../Hooks/useForm';
 import { TOKEN_POST, USER_GET } from '../../api';
+import { UserContext } from '../../UserContext';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
-
-  useEffect(() => {
-    const token = window.localStorage.getItem('token');
-    if (token) {     
-      getUser();
-    }
-  })
-
-  async function getUser(token){
-
-    const {url, options} = USER_GET(token);
-
-    const response = await fetch(url, options);
-    const json = await response.json();
-  }
+  const {userLogin, error, loading} = useContext(UserContext)
 
   async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
 
-      const {url, options} = TOKEN_POST({
-        username: username.value,
-        password: password.value
-      })
-
-      const response = await fetch(url, options);
-      const json = await response.json();
-
-      window.localStorage.setItem('token', json.token);
-       getUser(json.token);
+      userLogin(username.value, password.value)
       
     }
   }
@@ -49,7 +27,9 @@ const LoginForm = () => {
       <form action="" onSubmit={handleSubmit}>
         <Input type="text" label="Usuário" name='username' {...username} />
         <Input type="password" label="Senha" name='password' {...password} />
-        <Button>Entrar</Button>
+        {loading ? <button disabled>Carregando...</button> :
+        <Button>Entrar</Button>}
+        {error && <p>{error}</p>}
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
