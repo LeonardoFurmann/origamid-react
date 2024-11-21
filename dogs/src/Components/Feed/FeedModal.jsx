3768f482ac1ed9063c5494e4be19dbc1d4caa1e5
LeonardoFurmann <leohitnerf@gmail.com>
2024-11-21 19:05:25 -0300

    modal photo

--- dogs/src/Components/Feed/FeedModal.jsx
+++ dogs/src/Components/Feed/FeedModal.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import styles from './FeedModal.module.css'
+import useFetch from '../../Hooks/useFetch'
+import {PHOTO_GET} from '../../api'
+import Erro from '../Helper/Erro'
+import Loading from '../Helper/Loading'
+import PhotoContent from '../Photo/PhotoContent'
 
-const FeedModal = () => {
+const FeedModal = ({ photo, setModalPhoto }) => {
+
+  const {data, erro, loading, request} = useFetch();
+
+  useEffect(() => {
+    const {url, options} = PHOTO_GET(photo.id)
+    request(url, options);
+  },[photo, request])
+
+  function handleOutsideClick(event){
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  }
+
   return (
-    <div>FeedModal</div>
+    <div className={styles.modal} onClick={handleOutsideClick} >
+      {erro && <Erro error={erro}/>}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} /> }
+
+    </div>
   )
 }
 
