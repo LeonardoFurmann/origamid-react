4435d68a2547f4561debc7ab7ea9634cf1063546
Leonardo Furmann <leohitnerf@gmail.com>
2024-12-08 11:55:54 -0300

    erro 404

--- dogs/src/App.jsx
+++ dogs/src/App.jsx
@@ -10,6 +10,7 @@ import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
             <Route path='conta/*' element={<ProtectedRoute><User/></ProtectedRoute>} />
             <Route path='foto/:id' element={<ProtectedRoute><Photo/></ProtectedRoute>} />
             <Route path='perfil/:user' element={<ProtectedRoute><UserProfile/></ProtectedRoute>} />
+            <Route path='*' element={<NotFound />}/>
           </Routes>
           <Footer />
         </UserStorage>
---
+++ dogs/src/Components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='contaier mainContainer' >
+        <h1>Erro: 404</h1>
+        <p>Página não encontrada</p>
+    </div>
+  )
+}
+
+export default NotFound
